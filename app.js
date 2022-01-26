391f19384c0a5f4be6d1d4dd25c26d42fad14bcf
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-01-26 13:45:10 +0430

    add mobile navbar functionality

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-const btn = document.querySelector('.nav-icon');
-const sign = document.querySelector('.x-icon');
-const toggle = document.querySelector('.navbar');
+// const btn = document.querySelector('.nav-icon');
+// const sign = document.querySelector('.x-icon');
+// const toggle = document.querySelector('.navbar');
 
-btn.addEventListener('click', () => {
-  toggle.style.display = 'block';
-  btn.style.display = 'none';
-  sign.style.display = 'block';
-});
+// btn.addEventListener('click', () => {
+//   toggle.style.display = 'block';
+//   btn.style.display = 'none';
+//   sign.style.display = 'block';
+// });
 
-sign.addEventListener('click', () => {
-  sign.style.display = 'none';
-  btn.style.display = 'block';
-  toggle.style.display = 'none';
-});
+// sign.addEventListener('click', () => {
+//   sign.style.display = 'none';
+//   btn.style.display = 'block';
+//   toggle.style.display = 'none';
+// });
+
+const navSlide = () => {
+  const burger = document.querySelector(".nav-icon");
+  const nav = document.querySelector(".toggleUl");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    burger.classList.toggle("toggle");
+  });
+  document.querySelectorAll(".scrolList").forEach((n) =>
+    n.addEventListener("click", () => {
+      nav.classList.remove("nav-active");
+      burger.classList.remove("toggle");
+    })
+  );
+};
+
+navSlide();
