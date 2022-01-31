162ba5d5c833c7df148bd26fc502574318032138
fed1k <allamurotovcoder@mail.ru>
2022-02-01 02:22:26 +0500

    Mobile menu

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-const numpad = document.querySelector('name');
+const menuicon = document.querySelector('.menu-icon');
+const item1 = document.querySelector('.item1');
+const item2 = document.querySelector('.item2');
+const item3 = document.querySelector('.item3');
+const ul = document.querySelector('.navbar ul');
+
+menuicon.addEventListener('click', () => {
+  ul.classList.toggle('toggleUl');
+  item1.classList.toggle('items1');
+  item2.classList.toggle('items2');
+  item3.classList.toggle('items3');
+});
